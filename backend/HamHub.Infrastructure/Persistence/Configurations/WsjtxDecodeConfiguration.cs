c6cba361460f4b1deb5a1eb3a79ebf0db1c572dd
Micael Torp <mto@wiconsoft.com>
2026-06-13 12:39:35 +0200

    fix: add IsRequired to SpotterCallsign EF config

--- backend/HamHub.Infrastructure/Persistence/Configurations/WsjtxDecodeConfiguration.cs
+++ backend/HamHub.Infrastructure/Persistence/Configurations/WsjtxDecodeConfiguration.cs
@@ -10,7 +10,7 @@ public class WsjtxDecodeConfiguration : IEntityTypeConfiguration<WsjtxDecode>
     {
         builder.HasKey(d => d.Id);
         builder.Property(d => d.UserId).HasMaxLength(450).IsRequired();
-        builder.Property(d => d.SpotterCallsign).HasMaxLength(20);
+        builder.Property(d => d.SpotterCallsign).HasMaxLength(20).IsRequired();
         builder.Property(d => d.Message).HasMaxLength(30).IsRequired();
         builder.Property(d => d.DxCallsign).HasMaxLength(20);
         builder.Property(d => d.DxGrid).HasMaxLength(10);
